c7a98ec05b10bbdcef048da479e84f995b67d622
“simo1101” <vatinnosimona@gmail.com>
2025-10-12 13:34:14 +0200

    Translate comments and outputs from Italian to English
    All code comments, plot labels, and output messages in 'adaptive control/main.m', 'adaptive control/trace_figures.m', 'linear mpc/generateJacobian.m', and 'linear mpc/main.m' were translated from Italian to English for improved readability and maintainability. No functional changes were made to the code.

--- adaptive control/main.m
+++ adaptive control/main.m
@@ -2,49 +2,49 @@ clear all
 close all
 clc
 
-%% DATI DEL MANIPOLATORE
+%% MANIPULATOR PARAMETERS
 
-a1 = 1; a2 = 1;            % lunghezza del braccio [m]
-l1 = 0.5; l2 = 0.5;        % distanza dal centro di massa [m]
-ml1 = 50; ml2 = 50;        % massa del braccio [kg]
-Il1 = 10; Il2 = 10;        % momento d'inerzia del braccio [kg m^2]
-kr1 = 100; kr2 = 100;      % rapporto di riduzione del motore
-mm1 = 5; mm2 = 5;          % massa del motore [kg]
-Im1 = 0.01; Im2 = 0.01;    % momento d'inerzia del rotore [kg m^2]
-g = 9.81;                  % accelerazione di gravità [m/s^2]
-n = 2;                     % numero di giunti
-qi = [0; pi/4];            % valori iniziali delle varibili di giunto [rad]
-qf = [pi/2; pi/2];         % valori finali delle varibili di giunto [rad]
+a1 = 1; a2 = 1;            % arm length [m]
+l1 = 0.5; l2 = 0.5;        % distance from center of mass [m]
+ml1 = 50; ml2 = 50;        % arm mass [kg]
+Il1 = 10; Il2 = 10;        % arm inertia [kg m^2]
+kr1 = 100; kr2 = 100;      % motor gear ratio
+mm1 = 5; mm2 = 5;          % motor mass [kg]
+Im1 = 0.01; Im2 = 0.01;    % rotor inertia [kg m^2]
+g = 9.81;                  % gravitational acceleration [m/s^2]
+n = 2;                     % number of joints
+qi = [0; pi/4];            % initial joint variables [rad]
+qf = [pi/2; pi/2];         % final joint variables [rad]
 
-%% DATI DELLA SIMULAZIONE
-Tc = 0.005;                % tempo di campionamento [s]
-Tf = 5;                    % durata della simulazione [s]
-tempo = 0 : Tc : Tf;       % vettore dei tempi [s]
-nc = length(tempo);        % numero di campioni
+%% SIMULATION SETTINGS
+Tc = 0.005;                % sampling time [s]
+Tf = 5;                    % simulation duration [s]
+tempo = 0 : Tc : Tf;       % time vector [s]
+nc = length(tempo);        % number of samples
 
-%% GENERAZIONE DELLA TRAIETTORIA
+%% TRAJECTORY GENERATION
 
-% traiettoria con profilo di velocità trapezoidale
+% trajectory with trapezoidal velocity profile
 wpts1 = [0 pi/2]; wpts2 = [pi/4 pi/2]; 
 [q1ref, qd1ref, qdd1ref, ~] = trapveltraj(wpts1, nc, EndTime=Tf);
 [q2ref, qd2ref, qdd2ref, ~] = trapveltraj(wpts2, nc, EndTime=Tf);
 
-% % traiettoria polinomiale del terzo ordine
+% % third-order polynomial trajectory
 % tpts = [0 Tf];
 % [q1ref, qd1ref, qdd1ref, ~] = cubicpolytraj(wpts1, tpts, tempo);
 % [q2ref, qd2ref, qdd2ref, ~] = cubicpolytraj(wpts2, tpts, tempo);
 
-qref = [tempo; q1ref; q2ref]';       % posizione
-qdref = [tempo; qd1ref; qd2ref]';    % velocità
-qddref = [tempo; qdd1ref; qdd2ref]'; % accelerazione
+qref = [tempo; q1ref; q2ref]';       % position
+qdref = [tempo; qd1ref; qd2ref]';    % velocity
+qddref = [tempo; qdd1ref; qdd2ref]'; % acceleration
 
-%% MODELLAZIONE DELLA LOGICA DI CONTROLLO
+%% CONTROLLER LOGIC
 
 lambda = 5 * eye(n);
 Kd = 750 * eye(n);
 Kpi = 0.01 * eye(8);
 
-% vettore dei parametri iniziale, prima della variazione
+% initial parameter vector, before variation
 pi1 = ml1 + mm2;
 pi2 = ml1 * (l1 - a1);
 pi3 = Il1 + ml1 * (l1 - a1)^2 + Im2;
@@ -55,7 +55,7 @@ pi7 = Il2 + ml2 * (l2 - a2)^2;
 pi8 = Im2;
 pi = [pi1 pi2 pi3 pi4 pi5 pi6 pi7 pi8]';
 
-% vettore dei parametri finale, dopo la variazione
+% final parameter vector, after variation
 delta_m2 = 10;
 delta_m2lc2 = 11;
 delta_I2hat = 12.12;
@@ -64,16 +64,16 @@ pi5 = pi5 - delta_m2;
 pi6 = pi6 - delta_m2lc2;
 pi7 = pi7 - delta_I2hat;
 
-% vettore dei parametri finale, dopo la variazione, con tempi
+% final parameter vector, with time history
 I = ones(1, length(tempo));
 pi_t = [tempo; I*pi1; I*pi2; I*pi3; I*pi4; I*pi5; I*pi6; I*pi7; I*pi8]';
 
-%% SIMULAZIONE
+%% SIMULATION
 
 tic;
 sim('adaptive_control.slx')
 tf = toc;
 
-%% RISULTATI
+%% RESULTS
 
 trace_figures
--- adaptive control/trace_figures.m
+++ adaptive control/trace_figures.m
@@ -1,153 +1,153 @@
-%% SETTING PLOT FIGURE
-fsa     = 14;             % fontsize axis 14
-fsl     = 16;             % fontsize labels 18
-fsleg	= 14;		      % fontsize legenda
-font    = 'Times';        % tipo di font
+%% PLOT FIGURE SETTINGS
+fsa     = 14;             % axis fontsize
+fsl     = 16;             % label fontsize
+fsleg	= 14;		      % legend fontsize
+font    = 'Times';        % font type
 
 %%
 if tf <= 60
-    fprintf('Durata della simulazione: %.0f secondi.\n', tf)
+    fprintf('Simulation duration: %.0f seconds.\n', tf)
 else
-    fprintf('Durata della simulazione: %.0f minuti.\n', tf / 60)
+    fprintf('Simulation duration: %.0f minutes.\n', tf / 60)
 end
 
-%% Coppie ai giunti
+%% Joint torques
 tau1 = reshape(tau(1,1,:), 1, []);
 tau2 = reshape(tau(2,1,:), 1, []);
 
 figure(1)
 subplot(211), plot(tempo, tau1), grid on
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('\tau_1 [Nm]', 'fontsize', fsl, 'FontName', font)
 ylim([min(tau1), max(tau1)]), ax = gca; ax.YMinorGrid = 'on'; ax.YMinorTick = 'on';
 ax.YTick = linspace(min(ylim), max(ylim), 5); ax.YAxis.TickLabelFormat = '%.0f';
 
 subplot(212), plot(tempo, tau2), grid on
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('\tau_2 [Nm]', 'fontsize', fsl, 'FontName', font)
-sgtitle('Coppie ai giunti', 'fontsize', fsl)
+sgtitle('Joint torques', 'fontsize', fsl)
 ylim([min(tau2), max(tau2)]), ax = gca; ax.YMinorGrid = 'on'; ax.YMinorTick = 'on';
 ax.YTick = linspace(min(ylim), max(ylim), 5); ax.YAxis.TickLabelFormat = '%.0f';
 
-%% Variabili di giunto
+%% Joint variables
 q1 = reshape(q(1,1,:), 1, []);
 q2 = reshape(q(2,1,:), 1, []);
 
 figure(2)
 subplot(211), plot(tempo, q1), hold on, plot(tempo, q1ref), hold off
-grid on, xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+grid on, xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$q_1 [rad]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-legend({'Posizione', 'Riferimento'}, 'Interpreter','latex')	
+legend({'Position', 'Reference'}, 'Interpreter','latex')	
 legend('boxon'), set(legend, 'FontName', font, 'FontSize', fsleg)
 legend('Location','best')
 
 subplot(212), plot(tempo, q2), hold on, plot(tempo, q2ref), hold off
-grid on, xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+grid on, xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$q_2 [rad]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-legend({'Posizione', 'Riferimento'}, 'Interpreter','latex')	
+legend({'Position', 'Reference'}, 'Interpreter','latex')	
 legend('boxon'), set(legend, 'FontName', font, 'FontSize', fsleg)
 legend('Location','best')
-sgtitle('Variabili di giunto', 'fontsize', fsl)
+sgtitle('Joint variables', 'fontsize', fsl)
 
-%% Velocità dei giunti
+%% Joint velocities
 qd1 = reshape(qd(1,1,:), 1, []);
 qd2 = reshape(qd(2,1,:), 1, []);
 
 figure(3)
 subplot(211), plot(tempo, qd1), hold on, plot(tempo, qd1ref), hold off
-grid on, xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+grid on, xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\dot{q_1} [rad/s]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-legend({"Velocita'", 'Riferimento'}, 'Interpreter','latex')	
+legend({"Velocity", 'Reference'}, 'Interpreter','latex')	
 legend('boxon'), set(legend, 'FontName', font, 'FontSize', fsleg)
 legend('Location','best')
 
 subplot(212), plot(tempo, qd2), hold on, plot(tempo, qd2ref), hold off
-grid on, xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+grid on, xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\dot{q_2} [rad/s]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-legend({"Velocita'", 'Riferimento'}, 'Interpreter','latex')	
+legend({"Velocity", 'Reference'}, 'Interpreter','latex')	
 legend('boxon'), set(legend, 'FontName', font, 'FontSize', fsleg)
 legend('Location','best')
-sgtitle("Velocita' dei giunti", 'fontsize', fsl)
+sgtitle("Joint velocities", 'fontsize', fsl)
 
-%% Accelerazioni dei giunti
+%% Joint accelerations
 qdd1 = reshape(qdd(1,1,:), 1, []);
 qdd2 = reshape(qdd(2,1,:), 1, []);
 
 figure(4)
 subplot(211), plot(tempo, qdd1), hold on, plot(tempo, qdd1ref), hold off
-grid on, xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+grid on, xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\ddot{q_1} [rad/s^2]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-legend({'Accelerazione', 'Riferimento'}, 'Interpreter','latex')	
+legend({'Acceleration', 'Reference'}, 'Interpreter','latex')	
 legend('boxon'), set(legend, 'FontName', font, 'FontSize', fsleg)
 legend('Location','best'), ylim([min(qdd1), max(qdd1)])
 ax = gca; ax.YMinorGrid = 'on'; ax.YMinorTick = 'on';
 ax.YTick = linspace(min(ylim), max(ylim), 5); ax.YAxis.TickLabelFormat = '%.0f';
 
 subplot(212), plot(tempo, qdd2), hold on, plot(tempo, qdd2ref), hold off
-grid on, xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+grid on, xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\ddot{q_2} [rad/s^2]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-legend({'Accelerazione', 'Riferimento'}, 'Interpreter','latex')	
+legend({'Acceleration', 'Reference'}, 'Interpreter','latex')	
 legend('boxon'), set(legend, 'FontName', font, 'FontSize', fsleg)
 legend('Location','best'), ylim([min(qdd2), max(qdd2)])
-sgtitle('Accelerazioni dei giunti', 'fontsize', fsl)
+sgtitle('Joint accelerations', 'fontsize', fsl)
 ax = gca; ax.YMinorGrid = 'on'; ax.YMinorTick = 'on';
 ax.YTick = linspace(min(ylim), max(ylim), 5); ax.YAxis.TickLabelFormat = '%.0f';
 
-%% Norma dell'errore di posizione
+%% Position error norm
 figure(5)
 plot(tempo, pos_err_norm), grid on
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\Vert \tilde{q} \Vert [rad]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-title("Norma dell'errore di posizione", 'fontsize', fsl)
+title("Position error norm", 'fontsize', fsl)
 
-%% Norme dell'errore parametrico
+%% Parametric error norms
 figure(6)
 plot(tempo, param_err_norm), grid on
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
-ylabel('||errore parametrico||', 'fontsize', fsl, 'FontName', font)
-title("Norma dell'errore parametrico", 'fontsize', fsl)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
+ylabel('||parametric error||', 'fontsize', fsl, 'FontName', font)
+title("Parametric error norm", 'fontsize', fsl)
 
-%% Traiettorie di riferimento
+%% Reference trajectories
 figure(7)
 subplot(321), plot(tempo, q1ref), grid on, set(gca, 'fontsize', fsa)
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$q_1 [rad]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
 ax = gca; ax.YTick = linspace(min(q1ref), max(q1ref), 4); ax.YAxis.TickLabelFormat = '%.2f';
 ax.XTick = linspace(min(tempo), max(tempo), 5); ax.XAxis.TickLabelFormat = '%.1f';
 
 subplot(323), plot(tempo, qd1ref), grid on, set(gca, 'fontsize', fsa)
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\dot{q_1} [rad/s]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
 ax = gca; ax.YTick = linspace(min(qd1ref), max(qd1ref), 4); ax.YAxis.TickLabelFormat = '%.2f';
 ax.XTick = linspace(min(tempo), max(tempo), 5); ax.XAxis.TickLabelFormat = '%.1f';
 
 subplot(325), plot(tempo, qdd1ref), grid on, set(gca, 'fontsize', fsa)
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\ddot{q_1} [rad/s^2]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
 ax = gca; ax.YTick = linspace(min(qdd1ref), max(qdd1ref), 4); ax.YAxis.TickLabelFormat = '%.2f';
 ax.XTick = linspace(min(tempo), max(tempo), 5); ax.XAxis.TickLabelFormat = '%.1f';
 
 subplot(322), plot(tempo, q2ref), grid on, set(gca, 'fontsize', fsa)
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$q_2 [rad]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
 ax = gca; ax.YTick = linspace(min(q2ref), max(q2ref), 4); ax.YAxis.TickLabelFormat = '%.2f';
 ax.XTick = linspace(min(tempo), max(tempo), 5); ax.XAxis.TickLabelFormat = '%.1f';
 
 subplot(324), plot(tempo, qd2ref), grid on, set(gca, 'fontsize', fsa)
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\dot{q_2} [rad/s]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
 ax = gca; ax.YTick = linspace(min(qd2ref), max(qd2ref), 4); ax.YAxis.TickLabelFormat = '%.2f';
 ax.XTick = linspace(min(tempo), max(tempo), 5); ax.XAxis.TickLabelFormat = '%.1f';
 
 subplot(326), plot(tempo, qdd2ref), grid on, set(gca, 'fontsize', fsa)
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\ddot{q_2} [rad/s^2]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
 ax = gca; ax.YTick = linspace(min(qdd2ref), max(qdd2ref), 4); ax.YAxis.TickLabelFormat = '%.2f';
 ax.XTick = linspace(min(tempo), max(tempo), 5); ax.XAxis.TickLabelFormat = '%.1f';
 
-sgtitle('Riferimenti', 'fontsize', fsl)
+sgtitle('References', 'fontsize', fsl)
 
-%% Parametri stimati
+%% Estimated parameters
 pi_hat = squeeze(pi_hat);
 delta = pi_t(:, 2:end) - pi_hat';
 
@@ -155,18 +155,18 @@ figure(8)
 for i = 1 : 8
     subplot(4, 2, i), plot(tempo, delta(:, i))
     ylabel(strcat('\Delta_', num2str(i)), 'fontsize', fsl, 'FontName', font)
-    xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font), grid on
+    xlabel('Time [s]', 'fontsize', fsl, 'FontName', font), grid on
 end
-sgtitle('Differenza tra \pi stimato vs \pi vero', 'fontsize', fsl)
+sgtitle('Difference between estimated \pi and true \pi', 'fontsize', fsl)
 
-%% Norma dell'errore di velocità
+%% Velocity error norm
 figure(9)
 plot(tempo, vel_err_norm), grid on
-xlabel('Tempo [s]', 'fontsize', fsl, 'FontName', font)
+xlabel('Time [s]', 'fontsize', fsl, 'FontName', font)
 ylabel('$\Vert \tilde{\dot{q}} \Vert [rad/s]$', 'Interpreter', 'latex', 'FontSize', fsl, 'FontName', font)
-title("Norma dell'errore di velocita'", 'fontsize', fsl)
+title("Velocity error norm", 'fontsize', fsl)
 
-%% PER RAPPRESENTARE IL MANIPOLATORE
+%% TO REPRESENT THE MANIPULATOR
 figure("Color", "w")
 hPlot = helperRobotEnvironment(Length1 = a1, Length2 = a1);
 hPlot.Theta1 = qi(1); hPlot.Theta2 = qi(2); hPlot.xf = qf;
@@ -177,8 +177,8 @@ for k = 1 : nc
     drawnow limitrate
 end
 
-%% RISULTATI UTILI
-fprintf('Massimo errore di posizione in norma: %.3frad.\n', max(pos_err_norm))
-fprintf('Errore di posizione in norma per t=tf: %erad.\n', pos_err_norm(end))
-fprintf('Massimo errore di velocità in norma: %.3frad/s.\n', max(vel_err_norm))
-fprintf('Errore di velocità in norma per t=tf: %erad/s.\n', vel_err_norm(end))
+%% USEFUL RESULTS
+fprintf('Maximum position error (norm): %.3frad.\n', max(pos_err_norm))
+fprintf('Position error (norm) at t=tf: %erad.\n', pos_err_norm(end))
+fprintf('Maximum velocity error (norm): %.3frad/s.\n', max(vel_err_norm))
+fprintf('Velocity error (norm) at t=tf: %erad/s.\n', vel_err_norm(end))
